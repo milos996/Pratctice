d93421e63f401c669ec1798011c1cdf2c0b431cd
Milos Ivankovic <ivankovicmilos5@gmail.com>
2017-09-15 12:11:54 +0200

    Updating

---
+++ ConfigureR/client/index.js
@@ -0,0 +1,6 @@
+console.log("whats upp");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.js';
+
+ReactDOM.render(<App />, document.getElementById('root'));
