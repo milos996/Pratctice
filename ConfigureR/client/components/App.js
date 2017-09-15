311a3999cad83de383e005b12a7b5ca5083f2b87
Milos Ivankovic <ivankovicmilos5@gmail.com>
2017-09-15 13:10:31 +0200

    message

--- ConfigureR/client/components/App.js
+++ ConfigureR/client/components/App.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default class App extends React.Component {
   render() {
     return (
-     <div style={{textAlign: 'center'}}>
-        <h1>Hello World</h1>
+     <div style={{textAlign: 'center', fontSize: "80px"}}>
+        <h1>I love my lady the most in my life!!! </h1>
       </div>);
   }
 }
