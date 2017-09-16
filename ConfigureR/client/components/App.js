8057061e2ad9ff484b3674a4fda2651da23b28e5
Milos Ivankovic <ivankovicmilos5@gmail.com>
2017-09-16 10:53:17 +0200

    Adding bootstrap and stylesheet

--- ConfigureR/client/components/App.js
+++ ConfigureR/client/components/App.js
@@ -10,8 +10,37 @@ export default class App extends React.Component {
   render() {
     return (
      <div>
-        
+        <ul className="nav nav-tabs">
+            <li> <Link to="/"> Home </Link> </li>
+            <li> <Link to="/about"> About </Link> </li>
+            <li> <Link to="/topics"> Topics </Link> </li>
+        </ul>
+
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/topics" component={Topics} />
+
       </div>
     );
   }
+}
+
+
+function Home(props){
+    return(
+      <h1> Home </h1>
+    );
+}
+
+
+function About(props){
+  return(
+    <h1> About </h1>
+  );
+}
+
+function Topics(props){
+  return(
+    <h1> Topics </h1>
+  );
 }
